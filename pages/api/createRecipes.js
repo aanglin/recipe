e2b4fccfea2956ce20375617566ed61d098dd237
Aaron Anglin <aaron.anglin101@gmail.com>
2022-11-08 11:11:37 -0600

    added router

--- pages/api/createRecipes.js
+++ pages/api/createRecipes.js
@@ -1,15 +1,17 @@
 // import connectDB from "../../lib/mongodb";
 // import Recipe from "../../model/recipeSchema"
 import clientPromise from "../../lib/connect";
+import { useRouter } from "next/router";
 // connectDB();
 
 export default async function CreateRecipe(req, res) {
+    const router = useRouter();
     try{
         const client = await clientPromise;
   const db = client.db("myFirstDatabase");
   let myItem = req.body;
   let myRecipe = await db.collection("recipes").insertOne(myItem);
-        res.redirect('/homepage')
+        router.push('/homepage')
         if(!myRecipe){
             return res.json({"code": 'Recipe not saved'})
         }
