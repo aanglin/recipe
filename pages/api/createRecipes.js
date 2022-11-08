b85052d906cb8c5c7f8adb09e76c8123f7b08003
Aaron Anglin <aaron.anglin101@gmail.com>
2022-11-08 11:05:34 -0600

    changed post method

--- pages/api/createRecipes.js
+++ pages/api/createRecipes.js
@@ -1,18 +1,21 @@
 // import connectDB from "../../lib/mongodb";
 // import Recipe from "../../model/recipeSchema"
-
+import clientPromise from "../../lib/connect";
 // connectDB();
 
-// export default async function CreateRecipe(req, res) {
-//     try{
-//         const recipe = await Recipe.create(req.body);
-//         res.redirect('/homepage')
-//         if(!recipe){
-//             return res.json({"code": 'Recipe not saved'})
-//         }
-//     }catch(error){
-//         res.status(400).json({status: 'Not able to create recipe'})
-//     }
-// }
+export default async function CreateRecipe(req, res) {
+    try{
+        const client = await clientPromise;
+  const db = client.db("myFirstDatabase");
+  let myItem = req.body;
+  let myRecipe = await db.collection("recipes").insertOne(myItem);
+        res.redirect('/homepage')
+        if(!myRecipe){
+            return res.json({"code": 'Recipe not saved'})
+        }
+    }catch(error){
+        res.status(400).json({status: 'Not able to create recipe'})
+    }
+}
 
     
